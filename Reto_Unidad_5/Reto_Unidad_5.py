67a25b8e20a2761b797fe1946412b752e173a231
Sebastián Pulgarín C <sebastian.pulgarinc@upb.edu.co>
2025-10-28 19:39:57 -0500

    Adelanto Reto Unidad 5 - A

--- Reto_Unidad_5/Reto_Unidad_5.py
+++ Reto_Unidad_5/Reto_Unidad_5.py
@@ -3,7 +3,7 @@
 
 import csv
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # Creación de funcion CASE 1 MENÚ PRINCIPAL:
 
@@ -33,8 +33,9 @@ def LA():
 
 def CNPYC():
     nombre_archivo = input("Ingrese el nombre del archivo (con extensión, por ejemplo: texto.txt): ") # Pedimos al usuario el nombre del archivo junto con su extensión para trabajar sobre él.
-    archivo = open(nombre_archivo, "r", encoding="utf-8") # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
-    contenido = archivo.read() # Leemos los datos del archivo.
+    ruta = "./Reto_Unidad_5/" + nombre_archivo
+    with open(ruta, "r", encoding="utf-8") as archivo: # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
+        contenido = archivo.read() # Leemos los datos del archivo.
     num_palabras = len(contenido.split()) # Se cuenta el número de palabras. El método split permite separar una cadena de texto en elementos diferentes, en este caso, palabras.
     num_caracteres_total = len(contenido) # Se lee el número de caracteres. En ese caso no se usa split, porque no queremos que cada palabra sea un caracter. Se necesita contar cada letra, espacio, signo, etc.
     num_caracteres_sin_espacios = len(contenido.replace(" ", "").replace("\n", "")) # Se lee la cantidad de caracteres, sin incluir los espacios. Reemplazamos lo espacios y los enter para que ahora ya no existan al momento de contar.
@@ -43,12 +44,12 @@ def CNPYC():
     print(f"Número de palabras: {num_palabras}")
     print(f"Caracteres totales (incluyendo espacios): {num_caracteres_total}")
     print(f"Caracteres sin espacios: {num_caracteres_sin_espacios}")
-    archivo.close() # Se cierra el archivo para evitar conflictos al momento de querer modificarlo después.
 
 def RUPPO():
     nombre_archivo = input("Ingrese el nombre del archivo (con extensión, por ejemplo: texto.txt): ") # Pedimos al usuario el nombre del archivo junto con su extensión para trabajar sobre él.
-    archivo = open(nombre_archivo, "r", encoding="utf-8") # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
-    contenido = archivo.read() # Leemos los datos del archivo.
+    ruta = "./Reto_Unidad_5/" + nombre_archivo
+    with open(ruta, "r", encoding="utf-8") as archivo: # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
+        contenido = archivo.read() # Leemos los datos del archivo.
     palabra_buscar = input("Ingrese la palabra que desea reemplazar: ") # Se lee la palabra a reemplazar.
     palabra_reemplazo = input("Ingrese la nueva palabra: ") # Se lee la nueva palabra.
     ocurrencias = contenido.count(palabra_buscar) # Permite contar cuántas veces aparece la palabra antes de reemplazar.
@@ -56,7 +57,7 @@ def RUPPO():
         print(f"La palabra '{palabra_buscar}' no se encontró en el archivo.") # No se puede reemplazar una palabra que no está en el archivo.
     else:
         nuevo_contenido = contenido.replace(palabra_buscar, palabra_reemplazo) # Se reemplaza la palabra.
-        with open(nombre_archivo, "w", encoding="utf-8") as archivo: # Permite abrir el archivo, pero esta vez con la posibilidad de escribir en él. 
+        with open(ruta, "w", encoding="utf-8") as archivo: # Permite abrir el archivo, pero esta vez con la posibilidad de escribir en él. 
             archivo.write(nuevo_contenido) # Escribe las nuevas palabras en el archivo.
         print("=== REEMPLAZO COMPLETADO CON ÉXITO ===")
         print(f"Archivo: {nombre_archivo}")
@@ -66,7 +67,8 @@ def RUPPO():
 
 def HDODLV():
     nombre_archivo = input("Ingrese el nombre del archivo (con extensión, por ejemplo: texto.txt): ") # Pedimos al usuario el nombre del archivo junto con su extensión para trabajar sobre él.
-    with open(nombre_archivo, "r", encoding="utf-8") as archivo: # Abrimos el archivo como solo lectura y usamos la codificación adecuada para evitar problemas con tiles y letras ñ.
+    ruta = "./Reto_Unidad_5/" + nombre_archivo
+    with open(ruta, "r", encoding="utf-8") as archivo: # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
         contenido = archivo.read().lower() # Leemos el archivo. El método lower() para pasar todas las letras a minúscula y trabajar más cómodamente.
     vocales = ["a","e","i","o","u"] # Se crea una lista vacía donde se almacenarán las vocales. Agregamos las vocales iniciales para garantizar el orden en el histograma. Suma una letra de más a cada vocal, pero es irrelevante en la gráfica.
     for i in contenido: # Se usa un bucle para recorrer todas las letras del texto.
@@ -81,8 +83,10 @@ def HDODLV():
 # Creación de funciones CASE 3 MENÚ PRINCIPAL:
 
 def M15PF ():
-    nombre_archivo = input("Ingrese el nombre del archivo CSV (por ejemplo: datos.csv): ") # Pedimos al usuario el nombre del archivo junto con su extensión para trabajar sobre él.
-    archivo = open(nombre_archivo, "r", encoding="utf-8") # Se abre el archivo de texto. Se usa encoding="utf-8" para asegurar que la información se interprete en el formato correcto.
+    nombre_archivo = input("Ingrese el nombre del archivo CSV (por ejemplo: datos.csv): ")
+    ruta = "./Reto_Unidad_5/" + nombre_archivo
+    with open(ruta, "r", encoding="latin-1") as archivo: # Se abre el archivo de texto. Se usa encoding="latin-1" para asegurar que la información se interprete en el formato correcto.
+        contenido = archivo.read() # Leemos los datos del archivo.
     print("=== PRIMERAS QUINCE (15) FILAS DEL ARCHIVO ===")
     contador = 0 # Usamos un contador para garantizar que solo se mostrarán las primeras 15 líneas.
     for linea in archivo:
